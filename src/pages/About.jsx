2b00ecd057e4e828954d1c99d2719c9791499c3c
Jakobi McFadden <jakobimc574@gmail.com>
2025-06-30 18:15:39 -0500

    updated mobile about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,7 @@ function About() {
       <div className="flex flex-col items-center text-center pt-20 sm:hidden">
         <h1 className="text-5xl font-vcr-osd-mono font-bold text-white">about me</h1>
         <p 
-          className="mt-10 font-vcr-osd-mono text-white text-base leading-relaxed px-4"
+          className="mt-10 font-vcr-osd-mono text-white text-base leading-relaxed px-4 bg-black bg-opacity-50 rounded-lg shadow-lg"
         >
           hello! my name's jakobi, you can call me kobi for short, i'm a current college student 
           majoring in computer science at northern illinois university with an emphasis in 
@@ -18,8 +18,7 @@ function About() {
           skilled worker who enjoys devoting their time to furthering their skills and profiency
           in their techniques. collaboration is where i thrive as getting feedback from a different 
           set of eyes is always helpful and gets a project to completion. i hope to work with
-          a group of other like-minded individuals and create something worthwhile! 
-          <br />
+          a group of other like-minded individuals and create something worthwhile! <br />
           <br />
           check out my resume below!
         </p>
